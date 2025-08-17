9429a55f1fde6f7db779e02fb30d2541945eeeb4
savage-alpha <oliversikanga9@gmail.com>
2025-08-17 11:23:37 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,30 +1,41 @@
-require('dotenv').config();
+require('dotenv').config()
 
-global.APIs = {
-    xteam: 'https://api.xteam.xyz',
-    dzx: 'https://api.dhamzxploit.my.id',
-    lol: 'https://api.lolhuman.xyz',
-    violetics: 'https://violetics.pw',
-    neoxr: 'https://api.neoxr.my.id',
-    zenzapis: 'https://zenzapis.xyz',
-    akuari: 'https://api.akuari.my.id',
-    akuari2: 'https://apimu.my.id',
-    nrtm: 'https://fg-nrtm.ddns.net',
-    bg: 'http://bochil.ddns.net',
-    fgmods: 'https://api-fgmods.ddns.net'
-};
+// External API endpoints
+const APIs = {
+  xteam:    'https://api.xteam.xyz',
+  dzx:      'https://api.dhamzxploit.my.id',
+  lol:      'https://api.lolhuman.xyz',
+  violetics:'https://violetics.pw',
+  neoxr:    'https://api.neoxr.my.id',
+  zenzapis: 'https://zenzapis.xyz',
+  akuari:   'https://api.akuari.my.id',
+  akuari2:  'https://apimu.my.id',
+  nrtm:     'https://fg-nrtm.ddns.net',
+  bg:       'http://bochil.ddns.net',
+  fgmods:   'https://api-fgmods.ddns.net'
+}
 
-global.APIKeys = {
-    'https://api.xteam.xyz': 'd90a9e986e18778b',
-    'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
-    'https://api.neoxr.my.id': 'yourkey',
-    'https://violetics.pw': 'beta',
-    'https://zenzapis.xyz': 'yourkey',
-    'https://api-fgmods.ddns.net': 'fg-dylux'
-};
+// Corresponding API keys
+const APIKeys = {
+  'https://api.xteam.xyz':        'd90a9e986e18778b',
+  'https://api.lolhuman.xyz':     '85faf717d0545d14074659ad',
+  'https://api.neoxr.my.id':      'yourkey',
+  'https://violetics.pw':         'beta',
+  'https://zenzapis.xyz':         'yourkey',
+  'https://api-fgmods.ddns.net':  'fg-dylux'
+}
 
-module.exports = {
-    WARN_COUNT: 3,
-    APIs: global.APIs,
-    APIKeys: global.APIKeys
-};
+// Core bot settings
+const config = {
+  BOT_NAME:      'AidenBot MD',
+  OWNER_NAME:    'Oliver Sikanga',
+  FOOTER:        '⚡ Powered by AidenBot',
+  PREFIX:        '.',
+  WARN_COUNT:    3,
+  GROUP_LIMIT:   250,
+  AUTO_WELCOME:  true,
+  APIs,
+  APIKeys
+}
+
+module.exports = config
